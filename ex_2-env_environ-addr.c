767976e4819f502f5b3d0f4766dc832264b5fa09
Kidus Worku <kidusmik@gmail.com>
2021-10-24 09:36:33 +0000

    Modified: ex_2-env_environ-addr.c

--- ex_2-env_environ-addr.c
+++ ex_2-env_environ-addr.c
@@ -4,8 +4,8 @@ extern char **environ;
 
 int main(int ac, char **av, char **env)
 {
-	printf("Address of env: 0x%lu\n", (unsigned long)&env);
-	printf("Address of environ: 0x%lu\n", (unsigned long)&environ);
+	printf("Address of env: 0x%0lx\n", (unsigned long)&env);
+	printf("Address of environ: 0x%0lx\n", (unsigned long)&environ);
 
 	return (0);
 }
